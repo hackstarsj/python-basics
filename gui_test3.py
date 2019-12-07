ea5aebaaa96327670c2c894e832b88032dae1e73
sanjeev kumar <sksanjeev362@gmail.com>
2019-12-07 15:07:19 +0530

    Added GUI NotePad

--- gui_test3.py
+++ gui_test3.py
@@ -1,5 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow,QWidget,QLineEdit,QApplication,QVBoxLayout,QTextEdit,QAction,QFileDialog
+import os
+import json
+from PyQt5.QtWidgets import QMainWindow,QWidget,QLineEdit,QApplication,QVBoxLayout,QTextEdit,QAction,QFileDialog,QInputDialog,QTabWidget,QMenu
+from PyQt5.QtGui import QTextCursor
 
 def OpenClick():
     #print("Save")
@@ -10,8 +13,47 @@ def editDoc():
     file=QFileDialog.getOpenFileName(widget,"Open Single File","Default File","All Files(*)",options=option)
     fil=file[0]
     data=open(fil,"r")
+    finame=os.path.basename(data.name)
+    tablayout.setTabText(0,finame)
     linedit.setPlainText(data.read())
     data.close()
+
+def editDocMultiple():
+    option=QFileDialog.Options()
+    file=QFileDialog.getOpenFileNames(widget,"Open Single File","Default File","All Files(*)",options=option)
+    fil=file[0]
+    for fi in fil:
+        data=open(fi,"r+")
+        tab=QWidget()
+        layout=QVBoxLayout()
+        tab.setLayout(layout)
+        tex=QTextEdit()
+        layout.addWidget(tex)
+        layout.setContentsMargins(0,0,0,0)
+        tex.setPlainText(data.read())
+        name=os.path.basename(fi)
+        tablayout.addTab(tab,name)
+    #print(fil)
+    # data=open(fil,"r")
+    # finame=os.path.basename(data.name)
+    # tablayout.setTabText(0,finame)
+    # linedit.setPlainText(data.read())
+    # data.close()
+
+
+def initNotePad():
+    global file
+    if os.path.exists("./config.json"):
+        file=open("./config.json","r")
+        file_data=file.read()
+        file.close()
+        return file_data
+    else:
+        file_data={"size":17}
+        file=open("./config.json","w")
+        file.write(json.dumps(file_data))
+        file.close()
+        return file_data
 
 
 
@@ -32,11 +74,32 @@ def openSaveDialog():
     file.write(linedit.toPlainText())
     file.close()
 
+def settingFontSize():
+    global file_data
+    global datasiz
+    global file
+    data=QInputDialog.getInt(widget,"Font Size","Font Size",datasiz['size'],12,48,1)
+    if data[1]:
+
+        text=linedit.toPlainText()
+        file1=open("./config.json","w")
+        file_data={"size":data[0]}
+        file1.write(json.dumps(file_data))
+        file1.close()
+        datasiz=file_data
+
+
+        linedit.setPlainText("")
+        linedit.setFontPointSize(data[0])
+        linedit.setPlainText(text)
+
+
+global file
 app=QApplication(sys.argv)
-screen = app.primaryScreen()
-print('Screen: %s' % screen.name())
-size = screen.size()
-print('Size: %d x %d' % (size.width(), size.height()))
+#screen = app.primaryScreen()
+#print('Screen: %s' % screen.name())
+#size = screen.size()
+#print('Size: %d x %d' % (size.width(), size.height()))
 win=QMainWindow()
 
 menu=win.menuBar()
@@ -45,30 +108,63 @@ openMenu=menu.addMenu("File")
 openAction=QAction("Save")
 openAction.triggered.connect(OpenClick)
 openMenu.addAction(openAction)
+
 editment=menu.addMenu("Edit")
+
+
+open2=QMenu("Open M")
+openAction4=QAction("Open M")
+openAction4.triggered.connect(editDocMultiple)
+editment.addMenu(open2)
+open2.addAction(openAction4)
+
+open1=QMenu("Open")
 openAction2=QAction("Open")
 openAction2.triggered.connect(editDoc)
-editment.addAction(openAction2)
+editment.addMenu(open1)
+open1.addAction(openAction2)
 
 
-win.resize(size.width(),size.height())
+
+setting=menu.addMenu("Setting")
+openAction3=QAction("Font Size")
+openAction3.triggered.connect(settingFontSize)
+setting.addAction(openAction3)
+
+
+#win.resize(size.width(),size.height())
 widget=QWidget()
-widget.resize(size.width(),size.height())
+#widget.resize(size.width(),size.height())
 linedit=QTextEdit()
-linedit.resize(size.width(),size.height())
-linedit.setMaximumHeight(size.height())
-linedit.setMinimumHeight(size.height())
-linedit.setMinimumWidth(size.width())
-linedit.setMaximumWidth(size.width())
+#linedit.resize(size.width(),size.height())
+#linedit.setMaximumHeight(size.height())
+#linedit.setMinimumHeight(size.height())
+#linedit.setMinimumWidth(size.width())
+#linedit.setMaximumWidth(size.width())
+tablayout=QTabWidget()
 vertical=QVBoxLayout()
-linedit.setFontPointSize(17)
+file_data=initNotePad()
+#data1=(json.dumps(file_data['size']))
+#poi=data1['size']
+datasiz=json.loads(file_data)
+linedit.setFontPointSize(datasiz['size'])
 #vertical.resize(size.width(),size.height())
 vertical.addWidget(linedit)
 #vertical.addStretch()
 vertical.setContentsMargins(0,0,0,0)
-vertical.setSpacing(0)
-widget.setLayout(vertical)
+# vertical.setSpacing(0)
+tab1=QWidget()
+tab1.setLayout(vertical)
+tablayout.addTab(tab1,"Untitled")
+
+
+vertical1=QVBoxLayout()
+vertical1.setContentsMargins(0,0,0,0)
+vertical1.addWidget(tablayout)
+widget.setLayout(vertical1)
 win.setCentralWidget(widget)
 win.show()
 
+print(file_data)
+win.showMaximized()
 sys.exit(app.exec_())
---
+++ python_gui_example_project/Python_Notepad_Example.py
@@ -0,0 +1,90 @@
+import sys
+import os
+import json
+from PyQt5.QtWidgets import QWidget,QMainWindow,QTextEdit,QVBoxLayout,QApplication,QAction,QFileDialog,QInputDialog
+
+def openFile():
+    options=QFileDialog.Options()
+    data=QFileDialog.getOpenFileName(widget,"Select File","","All Files(*)",options=options)
+    fileopen=open(data[0],"r")
+    textbox.setPlainText(fileopen.read())
+    fileopen.close()
+
+
+def saveactionFun():
+    options=QFileDialog.Options()
+    data=QFileDialog.getSaveFileName(widget,"Save File","",options=options)
+    fileopen=open(data[0],"w")
+    fileopen.write(textbox.toPlainText())
+    fileopen.close()
+
+def initNotePad():
+    if os.path.exists("./config.json"):
+        data=open("./config.json","r")
+        dictdata=json.loads(data.read())
+        data.close()
+        return dictdata
+    else:
+        openfile=open("./config.json","w")
+        dictdata={"size":15}
+        openfile.write(json.dumps(dictdata))
+        openfile.close()
+        return dictdata
+
+def fontActionFun():
+    data=QInputDialog.getInt(widget,"Font Size","Font Size",initData['size'],12,48,1)
+    if data[1]:
+        initData['size']=data[0]
+        openfile=open("./config.json","w")
+        openfile.write(json.dumps(initData))
+        openfile.close()
+
+        textdata=textbox.toPlainText()
+        textbox.setPlainText("")
+        textbox.setFontPointSize(data[0])
+        textbox.setPlainText(textdata)
+
+
+
+
+app=QApplication(sys.argv)
+
+mainwindow=QMainWindow()
+mainwindow.setWindowTitle("Notepad")
+widget=QWidget()
+
+#menu
+menubar=mainwindow.menuBar()
+filemenu=menubar.addMenu("File")
+openaction=QAction("Open")
+openaction.triggered.connect(openFile)
+filemenu.addAction(openaction)
+
+saveaction=QAction("Save")
+saveaction.triggered.connect(saveactionFun)
+filemenu.addAction(saveaction)
+
+setting=menubar.addMenu("Setting")
+fontaction=QAction("Font Size")
+fontaction.triggered.connect(fontActionFun)
+setting.addAction(fontaction)
+
+
+layout=QVBoxLayout()
+layout.setContentsMargins(0,0,0,0)
+
+textbox=QTextEdit()
+
+initData=initNotePad()
+
+
+textbox.setFontPointSize(initData['size'])
+layout.addWidget(textbox)
+widget.setLayout(layout)
+
+mainwindow.setCentralWidget(widget)
+
+mainwindow.show()
+mainwindow.showMaximized()
+
+sys.exit(app.exec_())
